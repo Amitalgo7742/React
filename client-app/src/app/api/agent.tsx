60b3f5ec97c6c4d427b51ca66a458be0da12171e
Amitalgo7742 <7742196872amit@gmail.com>
2021-04-13 00:06:51 +0530

    chnages

--- client-app/src/app/api/agent.tsx
+++ client-app/src/app/api/agent.tsx
@@ -1,15 +1,43 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { SSL_OP_EPHEMERAL_RSA } from 'node:constants';
+import { toast } from 'react-toastify';
 import { Activity } from '../models/activity';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
-const sleep = (ms: number) => (response: AxiosResponse) => 
-    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+const sleep=(delay:number)=>{
+return new Promise((resolve)=>{
+    setTimeout(resolve,delay);
+})
+}
+
+axios.interceptors.response.use(async response=>{
+    await sleep(1000);
+    return response;
+},(error:AxiosError)=>{
+    const {data,status}=error.response!;
+    switch(status)
+    {
+        case 400: toast.error('Bad Request');
+        break;
+        case 401: toast.error('Unathourized');
+        break;
+        case 404: toast.error('Not Found');
+        break;
+        case 500: toast.error('Something Went Wrong');
+        break;
+
+    }
+    return Promise.reject(error);
+}
+)
+
+
 const responseBody = (response: AxiosResponse) => response.data;
 const requests = {
-    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody) 
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    del: (url: string) => axios.delete(url).then(responseBody) 
 };
 
 const Activities = {
