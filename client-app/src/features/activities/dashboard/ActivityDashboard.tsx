6b6bc5a4311038e36f826de2574ce6f5b032a481
Amitalgo7742 <7742196872amit@gmail.com>
2021-04-12 19:07:37 +0530

    changes

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,6 +7,7 @@ import ActivityForm from '../form/ActivityForm';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
+import ActivityFilters from './ActivityFilters';
 
 
 const ActivityDashboard: React.FC = ({
@@ -30,7 +31,7 @@ const ActivityDashboard: React.FC = ({
         />
       </Grid.Column>
       <Grid.Column width={6}>
-       <h2> Activity Filters</h2>
+      <ActivityFilters/>
       </Grid.Column>
     </Grid>
   );
