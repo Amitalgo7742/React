b86104b01728608e66f69ec71f85c38d97d73bde
Amitalgo7742 <7742196872amit@gmail.com>
2021-04-13 15:38:53 +0530

    chnages

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,15 @@
 import React, { useState, FormEvent, useEffect } from 'react';
-import { Segment,  Button } from 'semantic-ui-react';
+import { Segment, Button, FormField, Label } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
-import { Formik,Form,Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from '../../../app/common/form/MyTextArea';
 
 
 
@@ -29,6 +32,16 @@ const ActivityForm: React.FC = ({
     venue: ''
   });
 
+  const validationSchema = Yup.object({
+    title: Yup.string().required("Title of activity is required"),
+    category: Yup.string().required("Category of activity is required"),
+    description: Yup.string().required("Description of activity is required"),
+    date: Yup.string().required("Date of activity is required"),
+    city: Yup.string().required("City of activity is required"),
+    venue: Yup.string().required("Venu of activity is required"),
+
+  })
+
 
   useEffect(() => {
     if (id) loadActivity(id).then(activity => setActivity(activity!));
@@ -43,61 +56,31 @@ const ActivityForm: React.FC = ({
   return (
     <Segment clearing>
 
-      <Formik enableReinitialize initialValues={activity} onSubmit={value=>console.log(value)}>
-       {({values:activity,handleChange,handleSubmit})=>
-       (
-        <Form className="ui form" onSubmit={handleSubmit}>
-        <Field
-         
-          name='title'
-          placeholder='Title'
-         
-        />
-        <Field
-         
-          name='description'
-          rows={2}
-          placeholder='Description'
-         
-        />
-        <Field
-         
-          name='category'
-          placeholder='Category'
-         
-        />
-        <Field
-          
-          name='date'
-          type='datetime-local'
-          placeholder='Date'
-         
-        />
-        <Field
-         
-          name='city'
-          placeholder='City'
-         
-        />
-        <Field
-         
-          name='venue'
-          placeholder='Venue'
-         
-        />
-        <Button loading={loading} floated='right' positive type='submit' content='Submit' />
-        <Button
-          as={Link}
-          to='/activities'
-          floated='right'
-          type='button'
-          content='Cancel'
-        />
-      </Form>
-       )}
+      <Formik validationSchema={validationSchema}
+        enableReinitialize initialValues={activity} onSubmit={value => console.log(value)}>
+        {({ handleSubmit }) =>
+        (
+          <Form className="ui form" onSubmit={handleSubmit}>
+           <MyTextInput name="title" placeholder="Title"/>
+           <MyTextArea rows={3} name="description" placeholder="Description"/>
+           <MyTextInput name="category" placeholder="Category"/>
+           <MyTextInput name="date" placeholder="Date"/>
+           <MyTextInput name="city" placeholder="City"/>
+           <MyTextInput name="venue" placeholder="Venue"/>
+
+            <Button loading={loading} floated='right' positive type='submit' content='Submit' />
+            <Button
+              as={Link}
+              to='/activities'
+              floated='right'
+              type='button'
+              content='Cancel'
+            />
+          </Form>
+        )}
       </Formik>
 
-    
+
     </Segment>
   );
 };
